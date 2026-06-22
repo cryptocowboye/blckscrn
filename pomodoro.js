37174d6b4c7aad55e7ee285251fd5010e52071f2
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2026-06-21 21:14:24 -0400

    Turns pomodoro into a countdown and adds buttons

--- pomodoro.js
+++ pomodoro.js
@@ -1,9 +1,24 @@
 import { Timer } from 'easytimer.js';
 
 const timer = new Timer();
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn")
 
-timer.start({ startValues: { seconds: 10 } });
+
 timer.addEventListener('secondsUpdated', (e) => {
     document.getElementById('test').innerText = timer.getTimeValues().toString();
 });
+
+startBtn.addEventListener('click', () => {
+    timer.start({ countdown: true, startValues: { seconds: 25 * 60 } });
+})
+
+pauseBtn.addEventListener('click', () => {
+    timer.pause();
+})
+
+resetBtn.addEventListener('click', () => {
+    timer.reset();
+})
 
