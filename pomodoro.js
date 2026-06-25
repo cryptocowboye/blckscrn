4fad8646da942ca345fd76cee7519829eb1ce871
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2026-06-24 20:42:40 -0400

    Refactors to a class and function

--- pomodoro.js
+++ pomodoro.js
@@ -1,24 +1,45 @@
 import { Timer } from 'easytimer.js';
 
-const timer = new Timer();
-const startBtn = document.querySelector("#startBtn");
-const pauseBtn = document.querySelector("#pauseBtn");
-const resetBtn = document.querySelector("#resetBtn")
+class Pomodoro {
+    constructor() {
+        this.timer = new Timer();
+        this.cycles = 0;
+    }
+
+    getTime() {
+        return this.timer;
+    }
+
+    getCycle() {
+        return this.cycles;
+    }
+
+    startPomodoro() {
+        const [startBtn, pauseBtn, resetBtn] = [document.querySelector("#startBtn"), document.querySelector("#pauseBtn"), document.querySelector("#resetBtn")];
+
+        this.timer.addEventListener('secondsUpdated', () => {
+            document.getElementById('test').innerText = this.timer.getTimeValues().toString();
+        })
+
+        startBtn.addEventListener('click', () => {
+            this.timer.start({ countdown: true, startValues: { seconds: 25 * 60 } });
+        })
+
+        pauseBtn.addEventListener('click', () => {
+            this.timer.pause();
+        })
+
+        resetBtn.addEventListener('click', () => {
+            this.timer.reset();
+        })
+    }
+}
 
 
-timer.addEventListener('secondsUpdated', (e) => {
-    document.getElementById('test').innerText = timer.getTimeValues().toString();
-});
+function pomodoroMain() {
+    const pomo = new Pomodoro();
+    pomo.startPomodoro();
+}
 
-startBtn.addEventListener('click', () => {
-    timer.start({ countdown: true, startValues: { seconds: 25 * 60 } });
-})
-
-pauseBtn.addEventListener('click', () => {
-    timer.pause();
-})
-
-resetBtn.addEventListener('click', () => {
-    timer.reset();
-})
+pomodoroMain();
 
