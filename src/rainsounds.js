3df24a6067e4431a19b2176fa2a4ce7fa89cbbb4
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2026-06-28 23:08:23 -0400

    Adds a black screen webpage, but with rain

---
+++ src/rainsounds.js
@@ -0,0 +1,76 @@
+function rainNavBarFunctionality() {
+    const rainTrig = document.querySelector('#rain-header-trigger');
+    const rainHeader = document.querySelector('#rainsounds-header');
+    const rainNav = document.querySelector("#rainsounds-nav");
+
+    rainTrig.addEventListener('mouseenter', () => {
+        rainHeader.classList.remove('hidden');
+        rainHeader.classList.add('visible');
+
+        rainNav.style.visibility = 'visible';
+    });
+
+    rainTrig.addEventListener('mouseleave', () => {
+        rainHeader.classList.remove('visible');
+        rainHeader.classList.add('hidden');
+    })
+}
+
+async function rainWakeLockFunctionality() {
+
+    try {
+        rainWakeLock = await navigator.wakeLock.request('screen');
+    } catch (e){
+        console.error(`${e.name}: ${e.message}`);
+    }
+}
+
+function rainReapplyWakelock() {
+    document.addEventListener('visibilitychange', async () => {
+        if (rainWakeLock !== null && document.visibilityState === 'visible') {
+            await rainWakeLockFunctionality();
+        }
+    })
+}
+
+function rainFullScreen() {
+    const rainEl = document.querySelector('.blackscreen-main');
+
+    rainEl.addEventListener('dblclick', () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+            document.body.style.overflow = 'hidden';
+        } else if (document.exitFullscreen) {
+            document.body.style.overflow = 'auto';
+            document.exitFullscreen();
+        }
+    })
+}
+
+function playAudio() {
+    const pBtn = document.querySelector("#rain-player");
+    const rain = new Audio('./assets/rain.mp3');
+    rain.loop = true;
+    rain.volume = 0.5;
+
+    pBtn.addEventListener('click', () => {
+        if (rain.paused) {
+            rain.play();
+            pBtn.querySelector("#play-button").src = './assets/pause.png';
+            if (pBtn.id === 'rain-player') {
+                pBtn.id = 'rain-player-new'
+            }
+        } else {
+            rain.pause();
+            pBtn.querySelector('#play-button').src = './assets/play.png';
+        }
+    })
+}
+
+let rainWakeLock = null;
+
+rainFullScreen();
+rainWakeLockFunctionality();
+rainReapplyWakelock();
+rainNavBarFunctionality();
+playAudio();
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import Sitemap from 'vite-plugin-sitemap'
 
+
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                rainSounds: resolve(__dirname, 'rainsounds.html')
+            }
+        }
+    },
     plugins: [
         Sitemap({
             hostname: 'https://blkscreen.com',
