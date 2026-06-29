fd142ae796db77db6c2862b17b46637c4c9a3c26
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2026-06-29 00:32:00 -0400

    Alters paths as public refers to root

--- src/rainsounds.js
+++ src/rainsounds.js
@@ -1,3 +1,5 @@
+import sound from '/rain.mp3'
+
 function rainNavBarFunctionality() {
     const rainTrig = document.querySelector('#rain-header-trigger');
     const rainHeader = document.querySelector('#rainsounds-header');
@@ -49,20 +51,20 @@ function rainFullScreen() {
 
 function playAudio() {
     const pBtn = document.querySelector("#rain-player");
-    const rain = new Audio('public/rain.mp3');
+    const rain = new Audio(sound);
     rain.loop = true;
     rain.volume = 0.5;
 
     pBtn.addEventListener('click', () => {
         if (rain.paused) {
             rain.play();
-            pBtn.querySelector("#play-button").src = './public/pause.png';
+            pBtn.querySelector("#play-button").src = '/pause.png';
             if (pBtn.id === 'rain-player') {
                 pBtn.id = 'rain-player-new'
             }
         } else {
             rain.pause();
-            pBtn.querySelector('#play-button').src = './public/play.png';
+            pBtn.querySelector('#play-button').src = '/play.png';
         }
     })
 }
